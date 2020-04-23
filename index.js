7461cd21988bb21a6fdd55124dabd655bba20309
Jennifer Carballo <jcarballo@smu.edu>
2020-04-23 15:51:49 -0500

    cleaned and commented

--- index.js
+++ index.js
@@ -7,75 +7,92 @@
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
+
 /**
- * Respond with hello worker text
+ * Respond page according to variant
  * @param {Request} request
  */
 async function handleRequest(request) {
-  let cookie = request.headers.get("cookie");
-  const jsonResponse = await getResponse(
+  let cookie = request.headers.get("cookie"); //gets value for cookie whether it's set or not
+  const urls = await getResponse( 
     "https://cfw-takehome.developers.workers.dev/api/variants"
-  );
+  ); //stores array of varients in jsonResponse
 
-  let variNum = await getVariant(cookie);
-  const page = await fetch(jsonResponse[variNum]);
-  const newPage = new HTMLRewriter()
+  let variNum = await getVariant(cookie); //gets variant number from header or from random
+  const variantUrl = await fetch(urls[variNum]); //gets url of selected variant
+  const page = new HTMLRewriter() //uses Element handler class to add customizations
     .on("*", new ElementHandler(variNum))
-    .transform(page);
-  newPage.headers.set('Set-Cookie', variNum)
-  return newPage
+    .transform(variantUrl); //passes in url of selected variant
+  page.headers.set('Set-Cookie', variNum) //sets cookie to variant value
+  return page //returns the page of the generated variant
 }
 
+/**
+ * Fetches json from original url
+ * calls parseJSON
+ */
 async function getResponse(url) {
   const response = await fetch(url);
   return await parseJSON(response);
 }
 
+/**
+ * Returns current cookie value
+ * If there is no value set,
+ * randomly assign 0 or 1
+ */
 async function getVariant(cookieVal) {
-  console.log('in method:',cookieVal)
-  if (cookieVal == '0' || cookieVal == '1') {
+  if (cookieVal == '0' || cookieVal == '1') { //checks if one is set
     return cookieVal;
   }
   else {
-    //randomize variant for right now
+    //randomize variant if there is not one set
     let rand = Math.random(); //returns a number between 0 or 1
-    if (rand <= 0.5) cookieVal = 0;
-    else cookieVal = 1;
+    if (rand <= 0.5)
+      cookieVal = 0; //Variant 1
+    else
+      cookieVal = 1; //Variant 2
     return cookieVal;
   }
 }
 
+/**
+ * Returns the array with the 2 variant urls
+ */
 async function parseJSON(jsonResponse) {
-  const response = JSON.stringify(await jsonResponse.json());
-  let variants = JSON.parse(response).variants;
+  const response = JSON.stringify(await jsonResponse.json()); //converts to json string
+  let variants = JSON.parse(response).variants; //array of variant urls
   return variants;
 }
 
+/**
+ * Customization of variants
+ */
 class ElementHandler {
-  constructor(v) {
+  constructor(v) { //pass in generated variant number
     this.variant = v;
   }
   element(element) {
-    if (element.tagName === "title") {
+    if (element.tagName === "title") { //title in tab
       element.setInnerContent(`Jennifer Carballo's Cloudflare Application`);
-    } else if (element.tagName === "h1") {
+    } else if (element.tagName === "h1") { //title under checkmark with variant name
       element.prepend("This is ");
       element.append("!");
-    } else if (element.tagName === "p") {
+    } else if (element.tagName === "p") { //description under title; varies with variant
       if (this.variant == 0)
-        element.setInnerContent(
-          `What an interesting assignment, I learned a lot. Variant 2 has a link to my Github, clear cookies to access it!`
+        element.setInnerContent( //content for Variant 1
+          `What an interesting assignment, I learned a lot. Variant 2 has a link to my Github, clear cookies to access it! Check out my resume below :)`
         );
       else
-        element.setInnerContent(
-          `What an interesting assignment, I learned a lot. Variant 1 has a link to my Linkedin, clear cookies to access it!`
+        element.setInnerContent( //content for Variant 2
+          `What an interesting assignment, I learned a lot. Variant 1 has a link to my Linkedin, clear cookies to access it! Check out my previous projects below :)`
         );
-    } else if (element.tagName === "a") {
+    } else if (element.tagName === "a") { //link set on button; varies with variant
       if (this.variant == 0) {
-        element.setAttribute("href","https://www.linkedin.com/in/jennifer-carballo/");
+        element.setAttribute("href","https://www.linkedin.com/in/jennifer-carballo/"); //link for Variant 1
         element.setInnerContent(`Jennifer Carballo's Linkedin`);
       } else {
-        element.setAttribute("href", "https://github.com/jcarballo1");
+        element.setAttribute("href", "https://github.com/jcarballo1"); //linke for Variant 2
         element.setInnerContent(`Jennifer Carballo's Github`);
       }
     }
